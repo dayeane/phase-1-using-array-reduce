9866d30fece8ff601ab20b1772e097e46eb15bc1
Dayeane <dayeane85@gmail.com>
2021-10-13 23:18:49 -0700

    using the array reduce method

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+const { prototype } = require("mocha");
+
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+const reducer = (previousValue, currentValue) => previousValue + currentValue;
+
+const totalBatteries = batteryBatches.reduce(reducer)
+
+// copy and paste from Array prototype reduce.
